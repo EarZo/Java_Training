aa9136515d7d1600adf8c6fb96aa317e91bd5cc7
earzo <eranda19960417@gmail.com>
2020-03-02 06:34:08 +0530

    Created a seperate class for handling client requests by implementing HttpHandler interface. Status: In progress.

--- Exercises/SSL/HTTP/Server/src/com/eranda/Server.java
+++ Exercises/SSL/HTTP/Server/src/com/eranda/Server.java
@@ -5,37 +5,45 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Server {
 
     public static void main(String[] args) {
         try {
-//            URL url = new URL("http://[::1]:8082/");
-//            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//            con.setConnectTimeout(5000);
-//            con.setReadTimeout(5000);
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.print("Please enter the port to run the server: ");
+            int port = Integer.parseInt(scanner.nextLine());
 
-            HttpServer server = HttpServer.create(new InetSocketAddress(8082), 0);
-            HttpContext httpContext = server.createContext("/");
-            httpContext.setHandler(Server::handle);
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/", new MyHandler());
             server.start();
+            System.out.println("Server started successfully!");
         } catch (IOException e) {
             throw new RuntimeException("IO Exception occurred in the server side!", e);
         }
     }
 
-    static void handle(HttpExchange httpExchange) throws IOException {
-        System.out.println("Inside handle() method");
-        String response = "Hi there!";
-        httpExchange.sendResponseHeaders(200, response.getBytes().length);
-        OutputStream outputStream = httpExchange.getResponseBody();
-        outputStream.write(response.getBytes());
-        outputStream.close();
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            System.out.println("Inside handle() method");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()));
+            System.out.println(bufferedReader.readLine());
+            bufferedReader.close();
+
+            String response = "Hi there!";
+            httpExchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream outputStream = httpExchange.getResponseBody();
+            outputStream.write(response.getBytes());
+            outputStream.close();
+
+            httpExchange.close();
+        }
     }
+
 }
